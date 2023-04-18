e22c4aeae1bfa425397f63190368842638e5b797
Pranali Desai <pranali.desai@arbitersports.com>
2023-04-18 16:59:53 +0530

    button link added

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,5 +1,46 @@
-const Error = () => {
-    return <h1>Some Error Occured</h1>;
-  };
+import styled from "styled-components";
+import { Button } from "./Button";
+import { NavLink } from "react-router-dom";
 
-  export default Error;
+const Error = () => {
+    return (    
+    <Wrapper>
+      <div className="container">
+        <div>
+          <h2>404</h2>
+          <h2>Oh! You are lost.</h2>
+          <p>
+            The page you are looking for is not exists. Click to below button to go back to the homepage.          
+          </p>
+          <NavLink to="/" className="navbar-link">
+            <Button>Go Back to home</Button>
+          </NavLink>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+
+  .container {
+    padding: 9rem 0;
+    text-align: center;
+  }
+
+  h2 {
+    font-size: 5rem;
+  }
+
+  h3 {
+    font-size: 3rem;
+  }
+
+  p
+  {
+    margin: 2rem 0;
+  }
+
+`;
+
+export default Error;
